19b4f7bcad13cb59153a9c9a6d179345c94a107c
Baptiste Fagué <baptiste.fague@hotmail.com>
2025-11-25 20:33:47 +0100

    deck boilerplate

--- src/deck.rs
+++ src/deck.rs
@@ -2,7 +2,15 @@ pub struct Deck {
     deck: Vec<Card>,
 }
 
-impl Deck {}
+impl Deck {
+    fn draw_card(&mut self) -> Card {
+        todo!()
+    }
+
+    fn shuffle(&mut self, rng: &mut impl RngCore) {}
+
+    fn new() -> Self {}
+}
 
 #[derive(Clone, Copy)]
 pub struct Card {
@@ -21,4 +29,34 @@ pub enum Suit {
 #[derive(Clone, Copy)]
 pub struct Value {
     n: u32,
+}
+
+pub struct PlayerHand {
+    cards: [Card; 2],
+}
+
+pub struct PokerHand {
+    cards: [Card; 5],
+}
+
+impl Ord for PokerHand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        todo!()
+    }
+}
+
+pub struct Board {
+    cards: [Option<Card>; 5],
+}
+
+impl Board {
+    fn new() {
+        Board { cards: [None; 5] }
+    }
+
+    fn best_poker_hand(&self, player_hand: &PlayerHand) -> PokerHand {}
+
+    fn add_card(&mut self, card: Card) {}
+
+    fn card_count(&self) -> usize {}
 }
